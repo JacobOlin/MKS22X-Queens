3be92b69dbbf0dd95b4dcfd9ab4d92edb0bdcc8b
Jacob Olin <jolin00@stuy.edu>
2019-02-07 09:53:04 -0500

    Wrote the constructor and set all of the values in board to 0

--- QueenBoard.java
+++ QueenBoard.java
@@ -2,6 +2,11 @@ public class QueenBoard{
   private int[][]board;
 
   public QueenBoard(int size) {
-    
+    board = new int[size][size];
+    for (int i = 0;i < size;i += 1) {
+      for (int j = 0;j < size;j += 1) {
+        board[i][j] = 0;
+      }
+    }
   }
 }
