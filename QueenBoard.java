f0ccbaf62bc9890970c10fde05e91bde23b68082
Jacob Olin <jolin00@stuy.edu>
2019-02-08 11:26:01 -0500

    Fixed a bug in addQueen and removeQueen where it would only add to one square

--- QueenBoard.java
+++ QueenBoard.java
@@ -17,11 +17,11 @@ public class QueenBoard{
     board[r][c] = -1;
     for (int i = 1;i < board[r].length - c;i += 1) {
       board[r][c + i] += 1;
-      if (r - i > 0 ){
+      if (r - i >= 0 ){
         board[r - i][c + i] += 1;
       }
       if (r + i < board.length) {
-        board[r + 1][c + 1] += 1;
+        board[r + i][c + i] += 1;
       }
     }
     return true;
@@ -33,14 +33,30 @@ public class QueenBoard{
     }
     board[r][c] = 0;
     for (int i = 1;i < board[r].length - c;i += 1) {
-      board[r][c + 1] -= 1;
-      if (r - i > 0 ){
+      board[r][c + i] -= 1;
+      if (r - i >= 0 ){
         board[r - i][c + i] -= 1;
       }
       if (r + i < board.length) {
-        board[r + 1][c + 1] -= 1;
+        board[r + i][c + i] -= 1;
       }
     }
     return true;
+  }
+
+  public String testToString() {
+    String ans = "";
+    for (int i = 0;i < board.length;i += 1) {
+      for (int j = 0;j < board[i].length;j += 1) {
+        if (board[i][j] == -1) {
+          ans += "Q ";
+        }
+        else {
+          ans += board[i][j] + " ";
+        }
+      }
+      ans += "\n";
+    }
+    return ans;
   }
 }
